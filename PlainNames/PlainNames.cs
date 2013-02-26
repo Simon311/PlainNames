660994f4ed693c844d30ac473d1f030cbe462ccd
Simon <mkontent@gmail.com>
2013-02-26 14:24:31 +0400

    Initial commit

---
+++ PlainNames/PlainNames.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using TShockAPI;
+using Terraria;
+using Hooks;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace PlainNames
+{
+    [APIVersion(1, 12)]
+    public class PlainNames : TerrariaPlugin
+    {
+        public override Version Version
+        {
+            get { return new Version("1.0.0.0"); }
+        }
+
+        public override string Name
+        {
+            get { return "PlainNames"; }
+        }
+
+        public override string Author
+        {
+            get { return "Simon311"; }
+        }
+
+        public override string Description
+        {
+            get { return "Filters bad names"; }
+        }
+		
+        public PlainNames(Main game)
+            : base(game)
+        {
+            Order = -1;
+        }
+		
+        public override void Initialize()
+        {
+            ServerHooks.Join += OJ;
+        }
+		
+		protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerHooks.Join -= OJ;
+            }
+            base.Dispose(disposing);
+        }
+		
+        private void OJ(int ply, HandledEventArgs handler)
+        {
+			var player = TShock.Players[ply];
+			if (player == null)
+			{
+				handler.Handled = true;
+				return;
+			}
+			if(Regex.IsMatch(player.Name,"[^a-z0-9,. !]+", RegexOptions.IgnoreCase))
+            {
+                TShock.Utils.ForceKick(player, "Unacceptable name", true, false);
+				handler.Handled = true;
+				return;
+            }
+        }
+    }
+}
---
+++ PlainNames/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PlainNames")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("B4's Server")]
+[assembly: AssemblyProduct("PlainNames")]
+[assembly: AssemblyCopyright("Copyright © Simon311 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("92314305-3f43-41fe-9250-fb5aaa42755e")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
